2be5b0b548e28040f6102203912a947150a52b09
Celine Young <celine.kyoung@gmail.com>
2018-09-23 12:59:21 +1200

    Implemented route navigation so the app can now successfully navigate between screens.

--- photon/App.js
+++ photon/App.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from './src/navigation/TabNavigator';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { TabNavigator } from "./src/navigation/TabNavigator";
+import { createStackNavigator } from "react-navigation";
+import RegisterScreen from "./src/components/RegisterScreen/RegisterScreen";
+import LoginScreen from "./src/components/LoginScreen";
 
-type Props = {};
-export default class App extends Component<Props> {
+//type Props = {};
+
+const AppRouter = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Register: RegisterScreen,
+    Tabs: TabNavigator
+  },
+  {
+    initialRouteName: "Login"
+  }
+);
+
+export default class App extends React.Component {
   render() {
     return (
-      <View style={{
-        flex: 1,
-        backgroundColor: '#FFFFFF'
-    }}>
-        <TabNavigator style={[styles.tabBar]}/>
-    </View>
+      <View
+        style={{
+          flex: 1,
+          //backgroundColor: "#FFFFFF"
+        }}
+      >
+        {/* <TabNavigator style={[styles.tabBar]}/> */}
+        <AppRouter />
+      </View>
     );
   }
 }
@@ -20,9 +38,9 @@ const styles = StyleSheet.create({
   tabBar: {
     borderWidth: 1,
     borderRadius: 2,
-    borderColor: '#000000',
+    borderColor: "#000000",
     borderBottomWidth: 0,
-    shadowColor: '#000000',
+    shadowColor: "#000000",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
