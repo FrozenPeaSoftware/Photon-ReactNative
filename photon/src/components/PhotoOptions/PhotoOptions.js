2804f241b4bb8575ef96861dac3705eb943b5dff
Celine Young <celine.kyoung@gmail.com>
2018-09-26 22:30:55 +1200

    Rolled code back to working version, and incorporated new changes to Photo Options page.

--- photon/src/components/PhotoOptions/PhotoOptions.js
+++ photon/src/components/PhotoOptions/PhotoOptions.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Image, TouchableOpacity, TouchableHighlight, Text, KeyboardAvoidingView, FlatList } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+  Text,
+  KeyboardAvoidingView,
+  FlatList
+} from "react-native";
 import {
   FormLabel,
   FormInput,
@@ -8,71 +17,109 @@ import {
   Button
 } from "react-native-elements";
 
-import RNGooglePlaces from 'react-native-google-places';
+import RNGooglePlaces from "react-native-google-places";
 
 type Props = {};
 class PhotoOptions extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      locationQuery: '',
+      locationQuery: "",
       predictions: [],
 
-      location: '',
-      description: ''
+      location: "",
+      description: ""
     };
   }
 
-  locationQueryChanged = (locationQuery) => {
+  locationQueryChanged = locationQuery => {
     this.setState({
       locationQuery: locationQuery,
       location: locationQuery
     });
     console.log("changed");
-      RNGooglePlaces.getAutocompletePredictions(this.state.locationQuery)
-      .then((places) => {
+    RNGooglePlaces.getAutocompletePredictions(this.state.locationQuery)
+      .then(places => {
         // console.log(places);
-        this.setState({predictions: places});
+        this.setState({ predictions: places });
       })
       .catch(error => console.log(error.message));
-  }
+  };
 
   renderSearchResults = () => {
     if (this.state.locationQuery.length <= 3) {
       return;
     }
-    return <View style={styles.locationList}>
-    {
-      this.state.predictions.map(prediction => <TouchableOpacity onPress={() => locationItemPressed(prediction)} key={prediction.placeID} style={styles.locationListItem}><View><Text style={styles.locationListItemText}>{prediction.primaryText + ', ' + prediction.secondaryText}</Text></View></TouchableOpacity>)
-    }
-    </View>
-  }
+    return (
+      <View style={styles.locationList}>
+        {this.state.predictions.map(prediction => (
+          <TouchableOpacity
+            onPress={() => locationItemPressed(prediction)}
+            key={prediction.placeID}
+            style={styles.locationListItem}
+          >
+            <View>
+              <Text style={styles.locationListItemText}>
+                {prediction.primaryText + ", " + prediction.secondaryText}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
+    );
+  };
 
   render() {
-  let image = this.props.navigation.getParam('image');
-  if (image == undefined) {
-    image = {
-      path: "https://img2.cgtrader.com/items/273636/0cd44de4fd/large/winnie-the-pooh-rigged-t-pose-3d-model-low-poly-rigged-max.jpg"
+    let image = this.props.navigation.getParam("image");
+    if (image == undefined) {
+      image = {
+        path:
+          "https://img2.cgtrader.com/items/273636/0cd44de4fd/large/winnie-the-pooh-rigged-t-pose-3d-model-low-poly-rigged-max.jpg"
+      };
     }
-  }
-  return <View style={[styles.main]}>
-    <View style={[styles.centerContainer]}>
-      <FormLabel style={[styles.label]}>Location</FormLabel>
-      <FormInput inputStyle={[styles.formInput]} value={this.props.locationQuery} onChangeText={this.locationQueryChanged}/>
-      {this.renderSearchResults()}
-      <FormLabel style={[styles.label]}>Description</FormLabel>
-      <FormInput inputStyle={[styles.formInput]} onChangeText={(description) => this.setState({description})} value={this.state.description}/>
-      <Image style={[styles.imagePreview]} source={{uri: image.path}}/>
-      <View style={[styles.buttonsContainer]}>
-      <TouchableOpacity style={[styles.button]} onPress={() => this.props.navigation.goBack()}>
-          <Text style={styles.buttonText}>Cancel</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={[styles.button]} onPress={() => upload(this.props, image, this.state.location, this.state.description)}>
-          <Text style={styles.buttonText}>Upload</Text>
-      </TouchableOpacity>
+    return (
+      <View style={[styles.main]}>
+        <View style={[styles.centerContainer]}>
+          <FormLabel style={[styles.label]}>Location</FormLabel>
+          <View style={styles.locationSearchView}>
+            <FormInput
+              inputStyle={[styles.formInput]}
+              value={this.props.locationQuery}
+              onChangeText={this.locationQueryChanged}
+            />
+            {this.renderSearchResults()}
+          </View>
+          <FormLabel style={[styles.label]}>Description</FormLabel>
+          <FormInput
+            inputStyle={[styles.formInput]}
+            onChangeText={description => this.setState({ description })}
+            value={this.state.description}
+          />
+          <Image style={[styles.imagePreview]} source={{ uri: image.path }} />
+          <View style={[styles.buttonsContainer]}>
+            <TouchableOpacity
+              style={[styles.button]}
+              onPress={() => this.props.navigation.goBack()}
+            >
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button]}
+              onPress={() =>
+                upload(
+                  this.props,
+                  image,
+                  this.state.location,
+                  this.state.description
+                )
+              }
+            >
+              <Text style={styles.buttonText}>Upload</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-    </View>
-  </View>
+    );
   }
 }
 
@@ -82,8 +129,8 @@ function locationItemPressed(location) {
 
 function upload(props, image, location, description) {
   console.log("Uploading...");
-  console.log(location + ' ' + description);
-  props.navigation.navigate('Photo', {
+  console.log(location + " " + description);
+  props.navigation.navigate("Photo", {
     image: image,
     location: location,
     description: description
@@ -93,29 +140,29 @@ function upload(props, image, location, description) {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    justifyContent: 'space-evenly',
-    backgroundColor: 'white'
+    justifyContent: "space-evenly",
+    backgroundColor: "white"
   },
   centerContainer: {
-    height: '100%',
-    width: '100%',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    backgroundColor: 'white'
+    height: "100%",
+    width: "100%",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "white"
   },
   button: {
     backgroundColor: "#4ca7ed",
-    width: '42%',
+    width: "42%",
     margin: 10,
     height: 55,
     borderRadius: 50,
-    alignItems: 'center',
-    padding: 17,
+    alignItems: "center",
+    padding: 17
   },
   buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'flex-end',
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "flex-end",
     marginBottom: 10
   },
   buttonText: {
@@ -135,36 +182,41 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
     width: 375,
+    height: 50
   },
   label: {
     color: "#4ca7ed"
   },
   locationList: {
     zIndex: 1000,
-    position: 'absolute',
+    position: "absolute",
     backgroundColor: "#ececec",
-    marginTop: 90,
+    top: 50,
     borderRadius: 10,
     width: 375,
     shadowColor: "#000",
     shadowOffset: {
-	    width: 0,
-	    height: 4,
+      width: 0,
+      height: 4
     },
     shadowOpacity: 0.32,
     shadowRadius: 5.46,
-    elevation: 9,
+    elevation: 9
   },
   locationListItem: {
     padding: 20,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
     width: 375,
     height: 60
   },
   locationListItemText: {
     fontSize: 16
+  },
+  locationSearchView: {
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
