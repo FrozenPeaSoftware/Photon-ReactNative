a8f10bfd05dfd47a66dc7ca4d2b5f410b3f5210a
Leyton Blackler <leytonblackler@gmail.com>
2018-09-28 14:40:54 +1200

    Added support for displaying markers on the map.

--- photon/src/components/Map/Map.js
+++ photon/src/components/Map/Map.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Image, Dimensions, TouchableOpacity, Text, KeyboardAvoidingView, Modal } from "react-native";
 import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 
 class Map extends Component {
   render() {
     return <View style={styles.container}>
       <MapView style={styles.map} initialRegion = {{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421}}/>
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421}}>
+        <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324}}/>
+      </MapView>
     </View>
   }
 }
